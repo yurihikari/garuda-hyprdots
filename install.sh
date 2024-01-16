ecef3efdbe6c9a8f691bd507071a023eb3c04319
yurihikari <quangminhjean@gmail.com>
2024-01-16 22:27:26 +0100

    Force uninstall conflicting packages

--- install.sh
+++ install.sh
@@ -108,7 +108,7 @@ echo "Dependencies installed successfully"
 echo "Uninstalling conflicting packages"
 for i in "${conflicting_packages[@]}"; do
   if ! pacman -Qi $i &> /dev/null; then
-    sudo pacman -R $i --noconfirm
+    sudo pacman -Rdd $i --noconfirm
   fi
 done
 echo "Conflicting packages uninstalled successfully"
