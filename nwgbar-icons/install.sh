9ad3be0f22b001de020e5897f081b44d38e7db74
yurihikari <quangminhjean@gmail.com>
2024-01-17 22:42:20 +0100

    nwgbar script auto detect script directory

--- nwgbar-icons/install.sh
+++ nwgbar-icons/install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cp -r ./* /usr/share/nwg-launchers/nwgbar/images
+# Get this script directory 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cp -r $DIR/* /usr/share/nwg-launchers/nwgbar/images
